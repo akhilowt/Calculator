9555ac4c1b181f26fbcd22d9a25d07c74626ff4d
akhil <iamankushkumar@gmail.com>
2025-05-08 17:58:06 +0530

    test case added

---
+++ string-calculator/stringCalculator.test.js
@@ -0,0 +1,13 @@
+const add = require('./stringCalculator');
+
+test('returns 0 for empty string', () => {
+    expect(add("")).toBe(0);
+});
+
+test('returns number itself for single number', () => {
+    expect(add("5")).toBe(5);
+});
+
+test('returns sum for two numbers', () => {
+    expect(add("1,2")).toBe(3);
+});
